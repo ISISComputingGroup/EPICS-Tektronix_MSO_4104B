8fcfb14cc7a86c55b069fc612a59c7417a8fce96
Robert Nelson <robert.nelson@stfc.ac.uk>
2014-02-03 10:34:30 +0000

    Build IOC against asyn 4-22. Fix top level makefile. Use $(P) in opi
    Former-commit-id: a3172bb143297d2679db32a8cd1f9207f04c476f

--- iocs/Tektronix_MSO_4104B/iocBoot/iocTektronix_MSO_4104B/st.cmd
+++ iocs/Tektronix_MSO_4104B/iocBoot/iocTektronix_MSO_4104B/st.cmd
@@ -17,7 +17,7 @@ dbLoadDatabase "dbd/Tektronix_MSO_4104B.dbd"
 Tektronix_MSO_4104B_registerRecordDeviceDriver pdbbase
 
 #drvAsynIPPortConfigure ("IP", "130.246.50.156:4000")
-vxi11Configure("IP", "130.246.50.95", 0, 0.0,"inst0", 0, 0)
+vxi11Configure("IP", "130.246.50.172", 0, 0.0,"inst0", 0, 0)
 
 ## Load record instances
 dbLoadRecords("$(TOP)/../../db/devTektronix_MSO_4104B.db","P=$(IOCNAME), PORT=IP")
