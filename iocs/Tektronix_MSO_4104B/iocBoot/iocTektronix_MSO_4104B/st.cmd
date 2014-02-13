0756ae4a4259663313ad816859520ef84bd4a398
David Keymer <david.keymer@stfc.ac.uk>
2014-02-13 10:59:19 +0000

    updated IP address
    Former-commit-id: 01e2600aa75f7563cb5044c7904164beb844daba

--- iocs/Tektronix_MSO_4104B/iocBoot/iocTektronix_MSO_4104B/st.cmd
+++ iocs/Tektronix_MSO_4104B/iocBoot/iocTektronix_MSO_4104B/st.cmd
@@ -8,7 +8,7 @@
 epicsEnvSet "IOCNAME" "$(P=$(MYPVPREFIX))TEKMSO4104B"
 epicsEnvSet "IOCSTATS_DB" "$(DEVIOCSTATS)/db/iocAdminSoft.db"
 epicsEnvSet "STREAM_PROTOCOL_PATH" "$(TOP)/../../Tektronix_MSO_4104BSup"
-epicsEnvSet "EPICS_CA_MAX_ARRAY_BYTES" "6400000"
+epicsEnvSet "EPICS_CA_MAX_ARRAY_BYTES" "100000"
 
 cd ${TOP}
 
@@ -17,7 +17,11 @@ dbLoadDatabase "dbd/Tektronix_MSO_4104B.dbd"
 Tektronix_MSO_4104B_registerRecordDeviceDriver pdbbase
 
 #drvAsynIPPortConfigure ("IP", "130.246.50.156:4000")
+<<<<<<< .mine
+vxi11Configure("IP", "130.246.49.179", 0, 0.0,"inst0", 0, 0)
+=======
 vxi11Configure("IP", "130.246.50.172", 0, 0.0,"inst0", 0, 0)
+>>>>>>> .r962
 
 ## Load record instances
 dbLoadRecords("$(TOP)/../../db/devTektronix_MSO_4104B.db","P=$(IOCNAME), PORT=IP")
